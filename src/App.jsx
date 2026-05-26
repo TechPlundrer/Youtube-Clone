b1ec585eb80d9144256fcc044bea60ba868cb535
Uttam Gawas <uttam@whizti.com>
2026-05-26 13:49:15 +0530

    ShortLinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
+import {Route ,Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+
 
 const App = () => {
   return (
     <div>
        <Navbar/>
        <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home/>} />
           <Route path='/video/:categoryId/:videoId' element={<video/>} />
-          
+
        </Routes>
     </div>
   )
 }
 
-export default App
+export default App 
