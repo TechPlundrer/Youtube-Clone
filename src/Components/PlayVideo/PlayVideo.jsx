611a8d3f8a8b47aa938340ec839312eafbd88127
Uttam Gawas <uttam@whizti.com>
2026-05-27 21:15:09 +0530

    Suggest update

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PlayVideo.css";
 import video1 from "../../assets/video.mp4";
 import like from "../../assets/like.png";
@@ -7,12 +7,22 @@ import share from "../../assets/share.png";
 import save from "../../assets/save.png";
 import jack from "../../assets/jack.png";
 import user_profile from "../../assets/user_profile.jpg";
+import { API_KEY } from "../../data";
 
 const PlayVideo = ({videoId}) => {
+
+  const [apiData,setApiData] = useState(null);
+
+  const fetchVideoData = async ()=> {
+    //fetching videos data 
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+    await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]))
+  }
+
   return (
     <div className="play-video">
       {/* <video src={video1} controls autoPlay muted></video> */}
-      <iframe src={`https://www.youtube.com/embed/${videoId}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
       <h3>Best Youtube channel to learn development</h3>
       <div className="play-video-info">
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 import Recommended from '../../Components/Recommended/Recommended'
-
+import { useParams } from 'react-router-dom'
 
 const Video = () => {
 
-  const {videoId,categoryId} = useParems();
+  const {videoId,categoryId} = useParams();
+
   return (
     <div>
         <div className="play-container">
