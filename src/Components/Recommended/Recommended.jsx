e36ec4b8fa4f14332d9dffc9411fd92850b46500
Uttam Gawas <uttam@whizti.com>
2026-05-28 12:28:25 +0530

    updated sidebar Data

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,43 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Recommended.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
 import { API_KEY } from '../../data'
+import { value_converter } from '../../data'
 
 const Recommended = ({categoryId}) => {
 
     const [apiData,setApiData] = useState([]);
 
     const fetchData = async () => {
-        const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
-        await fetch(relatedVideo_url).then(res=>res.json()).then(data=>setApiData(data.items))
+        const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+        await fetch(relatedVideo_url)
+        .then(res=>res.json())
+        .then(data=>setApiData(data.items))
     }
 
     useEffect(()=> {
-            fetchData();
+        fetchData();
     },[])
 
   return (
     <div className='recommended'>
         {apiData.map((item,index)=> {
             return(
-                <div className="side-video-list">
-                <img src={thumbnail1} alt="" />
-                <div key={index} className="vid-info">
-                    <h4>Best channel to learn coding that help you to be a developer</h4>
-                    <p>Greatstack</p>
-                    <p>155k Views</p>
+                <div key={index} className="side-video-list">
+                    <img src={item.snippet.thumbnails.medium.url} alt="" />
+                    <div className="vid-info">
+                        <h4>{item.snippet.title}</h4>
+                        <p>{item.snippet.channelTitle}</p>
+                        <p>{value_converter(item.statistics.viewCount)} Views</p>
+                    </div>
                 </div>
-            </div>
-            )
+            ) 
         })}
-       
     </div>
   ) 
 }
